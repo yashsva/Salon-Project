0246c09386161f2c6ad2fed2ac71d0d0b598696c
Yashsva <namansisodiya2013@gmail.com>
2021-07-19 20:05:03 +0530

    payment integration and authentication of routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router=express.Router();
 
 const auth_controller=require('../controllers/auth');
+const auth=require('../middlewares/auth');
 
-router.get('/logout',auth_controller.logout);
+
+router.get('/logout',auth.verify_loggedIn,auth_controller.logout);
 
 module.exports=router;
--- routes/customer.js
+++ routes/customer.js
@@ -1,17 +1,19 @@
 const express = require('express');
 
 const customer_control=require('../controllers/customer');
+const auth=require('../middlewares/auth');
+
 
 const router=express.Router();
 
-router.get('/signup',customer_control.get_signup);
+router.get('/signup',auth.verify_not_loggedIn,customer_control.get_signup);
 
-router.post('/signup',customer_control.post_signup);
+router.post('/signup',auth.verify_not_loggedIn,customer_control.post_signup);
 
-router.get('/login',customer_control.get_login);
+router.get('/login',auth.verify_not_loggedIn,customer_control.get_login);
 
-router.post('/login',customer_control.post_login);
+router.post('/login',auth.verify_not_loggedIn,customer_control.post_login);
 
-router.get('/bookings',customer_control.get_bookings)
+router.get('/bookings',auth.verify_customer,customer_control.get_bookings)
 
 module.exports=router;
