e325b7bc78e1b355bd859c6feb6dafcb9b10a69c
Yashsva <namansisodiya2013@gmail.com>
2021-09-03 13:36:27 +0530

    cloud configuration changes

--- utils/database.js
+++ utils/database.js
@@ -3,12 +3,17 @@ const mysql = require('mysql2');
 //To get API keys from .env files (Using process.env.<variable_name> )
 require('dotenv').config();
 
+const ssl_cert=fs.readFileSync(path.join(__dirname,"..","DigiCertGlobalRootCA.crt.pem"));
+
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     multipleStatements: true,
+    ssl:{
+        ca:ssl_cert,
+    }
 })
 
 module.exports = pool.promise();
